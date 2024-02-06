ffb2e79ee49aa3f35f980309e07a90dd74e74f42
eliott-guyot <eliott.guyot@etu.univ-orleans.fr>
2024-02-06 16:33:16 +0100

    Biblio Box: ajout de test_box_add

--- TestsBoxes.java
+++ TestsBoxes.java
@@ -5,4 +5,11 @@ public class TestsBoxes{
     public void testBoxCreate() {
     Box b = new Box();
     }
-};
+    /** on veut pouvoir mettre des trucs dedans */
+    @Test
+    public void testBoxAdd(){
+        Box b = new Box();
+        b.add("truc1");
+        b.add("truc2");
+}
+}
